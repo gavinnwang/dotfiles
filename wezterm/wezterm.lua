d6f2cc70f872553cea9104fe959371d140346f49
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2024-05-14 20:17:29 -0500

    changes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -16,8 +16,8 @@ config.window_padding = {
 	top = 0,
 	bottom = 0,
 }
--- config.font =
---   wezterm.font('JetBrains Mono', { weight = 500, italic = false })
+config.font =
+  wezterm.font('JetBrains Mono', { weight = 200, italic = false })
 
 config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
 
@@ -38,27 +38,27 @@ local direction_keys = {
   l = 'Right',
 }
 
-local function split_nav(resize_or_move, key)
-  return {
-    key = key,
-    mods = resize_or_move == 'resize' and 'META' or 'CTRL',
-    action = w.action_callback(function(win, pane)
-      if is_vim(pane)  then
-        -- pass the keys through to vim/nvim
-        win:perform_action({
-          SendKey = { key = key, mods = resize_or_move == 'resize' and 'META' or 'CTRL' },
-        }, pane)
-      else
-        if resize_or_move == 'resize' then
-          win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
-        else
-          win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
-        end
-      end
-    end),
-  }
-end
-
+-- local function split_nav(resize_or_move, key)
+--   return {
+--     key = key,
+--     mods = resize_or_move == 'resize' and 'META' or 'CTRL',
+--     action = w.action_callback(function(win, pane)
+--       if is_vim(pane)  then
+--         -- pass the keys through to vim/nvim
+--         win:perform_action({
+--           SendKey = { key = key, mods = resize_or_move == 'resize' and 'META' or 'CTRL' },
+--         }, pane)
+--       else
+--         if resize_or_move == 'resize' then
+--           win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+--         else
+--           win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+--         end
+--       end
+--     end),
+--   }
+-- end
+--
 config.keys= {
 	{
 		key = "w",
@@ -66,15 +66,15 @@ config.keys= {
 		action = act.CloseCurrentTab({ confirm = false }),
 	},
     -- move between split panes
-    split_nav('move', 'h'),
-    split_nav('move', 'j'),
-    split_nav('move', 'k'),
-    split_nav('move', 'l'),
-    -- resize panes
-    split_nav('resize', 'h'),
-    split_nav('resize', 'j'),
-    split_nav('resize', 'k'),
-    split_nav('resize', 'l'),
+    -- split_nav('move', 'h'),
+    -- split_nav('move', 'j'),
+    -- split_nav('move', 'k'),
+    -- split_nav('move', 'l'),
+    -- -- resize panes
+    -- split_nav('resize', 'h'),
+    -- split_nav('resize', 'j'),
+    -- split_nav('resize', 'k'),
+    -- split_nav('resize', 'l'),
 	{
 		key = "h",
 		mods = "SHIFT|CMD",
