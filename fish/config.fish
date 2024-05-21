2a964567230b1803c47a5afce582484b29fa6225
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2024-05-21 17:23:05 -0500

    changes

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,4 @@
 fish_add_path /usr/local/bin
-fish_add_path ~/bin
 fish_add_path /opt/homebrew/bin
 fish_add_path /opt/homebrew/opt/node@20/bin
 
@@ -12,9 +11,12 @@ function finder
 end
 
 zoxide init fish | source
-
+thefuck --alias | source
+fzf --fish | source
 starship init fish | source
 
+alias t tmux
+alias ta 'tmux attach || tmux new'
 alias ls='eza --icons -F -H --group-directories-first --git'
 alias cat bat
 alias v nvim
@@ -23,7 +25,9 @@ alias wez='wezterm cli'
 alias rm trash
 alias config='/usr/bin/git --git-dir=/Users/gavinwang/.cfg/ --work-tree=/Users/gavinwang/.config'
 
-thefuck --alias | source
+# option l to search git log
+fzf_configure_bindings --git_log=\el --git_status=\es --directory=\ef --processes=\ep --variables=\ev
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
