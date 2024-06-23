f9c7000e8d5b044c88862708688a464815d73377
Gavin <79772347+gavinnwang@users.noreply.github.com>
2024-06-23 00:46:32 -0700

    Changes

--- nvim/.config/nvim/lua/gwang/lazy/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-  { 'numToStr/Comment.nvim', opts = {} },
-  {
-    'ray-x/lsp_signature.nvim',
-    event = 'VeryLazy',
-    opts = {},
-  },
-}
