ec93edc91505f79cb7b59e4f56b77763935c29f2
Gavin <79772347+gavinnwang@users.noreply.github.com>
2024-06-24 01:45:46 -0700

    Changes

---
+++ nvim/.config/nvim/lua/gwang/lazy/indent-blankline.lua
@@ -0,0 +1,12 @@
+return {
+  'lukas-reineke/indent-blankline.nvim',
+  opts = {
+    indent = { char = '│' },
+    scope = { char = '│' },
+  },
+  config = function(_, opts)
+    local hooks = require 'ibl.hooks'
+    hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+    require('ibl').setup(opts)
+  end,
+}
