0a09b7db4a206275737cfd38a56d181bcccc8c38
Gavin <79772347+gavinnwang@users.noreply.github.com>
2024-06-24 02:42:24 -0700

    Changes

--- nvim/.config/nvim/lua/gwang/lazy/cmp.lua
+++ nvim/.config/nvim/lua/gwang/lazy/cmp.lua
@@ -43,33 +43,32 @@ return { -- Autocompletion
   'hrsh7th/nvim-cmp',
   event = 'InsertEnter',
   dependencies = {
-    -- Snippet Engine & its associated nvim-cmp source
+    -- -- Snippet Engine & its associated nvim-cmp source
+    -- {
+    --   'L3MON4D3/LuaSnip',
+    --   build = (function()
+    --     if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+    --       return
+    --     end
+    --     return 'make install_jsregexp'
+    --   end)(),
+    --   dependencies = {},
+    -- },
     {
-      'L3MON4D3/LuaSnip',
-      build = (function()
-        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-          return
-        end
-        return 'make install_jsregexp'
-      end)(),
-      dependencies = {
-        {
-          'zbirenbaum/copilot-cmp',
-          config = function()
-            require('copilot_cmp').setup()
-          end,
-        },
-      },
+      'zbirenbaum/copilot-cmp',
+      config = function()
+        require('copilot_cmp').setup()
+      end,
     },
-    'saadparwaiz1/cmp_luasnip',
+    -- 'saadparwaiz1/cmp_luasnip',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
-    'hrsh7th/cmp-buffer',
+    -- 'hrsh7th/cmp-buffer',
   },
   config = function()
     local cmp = require 'cmp'
-    local luasnip = require 'luasnip'
-    luasnip.config.setup {}
+    -- local luasnip = require 'luasnip'
+    -- luasnip.config.setup {}
 
     cmp.setup {
       formatting = {
@@ -92,11 +91,11 @@ return { -- Autocompletion
           scrollbar = false,
         },
       },
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
+      -- snippet = {
+      --   expand = function(args)
+      --     luasnip.lsp_expand(args.body)
+      --   end,
+      -- },
       completion = { completeopt = 'menu,menuone,noinsert' },
       mapping = cmp.mapping.preset.insert {
         ['<CR>'] = cmp.mapping.confirm { select = true },
@@ -107,8 +106,8 @@ return { -- Autocompletion
       sources = {
         { name = 'nvim_lsp' },
         { name = 'path' },
-        { name = 'buffer' },
-        { name = 'luasnip' },
+        -- { name = 'buffer' },
+        -- { name = 'luasnip' },
         { name = 'copilot' },
       },
     }
