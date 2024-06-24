eccd36a3d46a0aac464e8f64191ed5d1463e8a9d
Gavin <79772347+gavinnwang@users.noreply.github.com>
2024-06-23 23:07:27 -0700

    Changes

--- nvim/.config/nvim/lua/gwang/lazy/nvim-tree.lua
+++ nvim/.config/nvim/lua/gwang/lazy/nvim-tree.lua
@@ -16,8 +16,8 @@ return {
     },
     view = {
       adaptive_size = false,
-      side = 'left',
-      width = 30,
+      side = 'right',
+      width = 40,
       preserve_window_proportions = true,
     },
     git = {
