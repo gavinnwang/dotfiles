5f7f8a98e3212fa1d1b46a24074fa304385c6e7e
Gavin <79772347+gavinnwang@users.noreply.github.com>
2024-06-26 18:08:48 -0500

    1

--- nvim/.config/nvim/lua/gwang/lazy/colorscheme.lua
+++ nvim/.config/nvim/lua/gwang/lazy/colorscheme.lua
@@ -58,13 +58,9 @@
 
 return {
   {
-    -- "folke/tokyonight.nvim",
-    -- "AlphaTechnolog/pywal.nvim",
-    -- "rockerBOO/boo-colorscheme-nvim",
     'jesseleite/nvim-noirbuddy',
-    -- "tiagovla/tokyodark.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    lazy = false,
+    priority = 1000,
     dependencies = {
       {
         'rktjmp/lush.nvim',
@@ -72,28 +68,23 @@ return {
       { 'tjdevries/colorbuddy.nvim', branch = 'dev' },
     },
     opts = {
-      -- transparent_background = false,
-      -- gamma = 0.85,
-      -- styles = {
-      --   comments = { italic = false },
-      --   keywords = { italic = false },
-      --   identifiers = { italic = false },
-      --   functions = { italic = false },
-      --   variables = { italic = false }
-      -- }
+      transparent_background = false,
+      gamma = 0.85,
+      styles = {
+        comments = { italic = false },
+        keywords = { italic = false },
+        identifiers = { italic = false },
+        functions = { italic = false },
+        variables = { italic = false },
+      },
+      -- colors = {
+      --   primary = '#EE579B',
+      --   secondary = '#93518B',
+      -- },
       colors = {
-        primary = '#E4579B',
-        secondary = '#93518B',
+        secondary = '#6EE2FF',
+        primary = '#267FB5',
       },
     },
-    config = function(_, opts)
-      require('noirbuddy').setup(opts)
-      -- require('noirbuddy').setup {
-      --   colors = {
-      --     primary = '#6EE2FF',
-      --     secondary = '#267FB5',
-      --   },
-      -- }
-    end,
   },
 }
