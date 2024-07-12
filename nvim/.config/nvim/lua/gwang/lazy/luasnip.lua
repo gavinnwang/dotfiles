f79fd62868f6ec8e0e7fe8c331dc0af473f1d9bf
Gavin <79772347+gavinnwang@users.noreply.github.com>
2024-07-12 05:08:30 -0500

    Changes

--- nvim/.config/nvim/lua/gwang/lazy/luasnip.lua
+++ nvim/.config/nvim/lua/gwang/lazy/luasnip.lua
@@ -28,5 +28,87 @@ return {
         },
       }),
     })
+    ls.add_snippets('cpp', {
+      s('dfs graph', {
+        t {
+          'vector<bool> vis;',
+          'void dfs(int u) {',
+          '  vis[u] = 1;',
+          '  for (int v : adj[u]) {',
+          '    if (!vis[v]) {',
+          '      dfs(v);',
+          '    }',
+          '  }',
+          '}',
+        },
+      }),
+    })
+    ls.add_snippets('cpp', {
+      s('dfs tree', {
+        t {
+          'void dfs(int u, int p = -1) {',
+          '  for (int v : adj[u]) {',
+          '    if (v == p) continue;',
+          '    dfs(v, u);',
+          '  }',
+          '}',
+        },
+      }),
+    })
+    ls.add_snippets('cpp', {
+      s('vv', {
+        t {
+          'vector<int> v(n);',
+          'for (int i = 0; i < n; i++) {',
+          '  cin >> v[i];',
+          '}',
+        },
+      }),
+    })
+    ls.add_snippets('cpp', {
+      s('vn', {
+        t {
+          'int n;',
+          'cin >> n;',
+          'vector<int> v(n);',
+          'for (int i = 0; i < n; i++) {',
+          '  cin >> v[i];',
+          '}',
+        },
+      }),
+    })
+    ls.add_snippets('cpp', {
+      s('nm', {
+        t {
+          'int n, m;',
+          'cin >> n >> m;',
+        },
+      }),
+    })
+    ls.add_snippets('cpp', {
+      s('dsu', {
+        t {
+          'struct DSU {',
+          '  vector<int> sz;',
+          '  vector<int> pa;',
+          '  DSU(int size) : sz(size, 1), pa(size) {',
+          '    for (int i = 0; i < size; i++) {',
+          '      pa[i] = i;',
+          '    }',
+          '  };',
+          '  int find(int x) { return pa[x] == x ? x : (pa[x] = find(pa[x])); }',
+          '  bool unite(int x, int y) {',
+          '    int xp = find(x);',
+          '    int yp = find(y);',
+          '    if (xp == yp) return false;',
+          '    if (sz[xp] < sz[yp]) swap(xp, yp);',
+          '    sz[xp] += sz[yp];',
+          '    pa[yp] = xp;',
+          '    return true;',
+          '  }',
+          '};',
+        },
+      }),
+    })
   end,
 }
--- nvim/.config/nvim/lua/gwang/lazy/refactoring.lua
+++ nvim/.config/nvim/lua/gwang/lazy/refactoring.lua
@@ -1,16 +1,16 @@
 return {
-  lazy = true,
-  'ThePrimeagen/refactoring.nvim',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-treesitter/nvim-treesitter',
-  },
-  config = function()
-    ---@diagnostic disable-next-line: missing-fields
-    require('refactoring').setup {
-      vim.keymap.set('x', '<leader>re', function()
-        require('refactoring').refactor 'Extract Function'
-      end),
-    }
-  end,
+  -- lazy = true,
+  -- 'ThePrimeagen/refactoring.nvim',
+  -- dependencies = {
+  --   'nvim-lua/plenary.nvim',
+  --   'nvim-treesitter/nvim-treesitter',
+  -- },
+  -- config = function()
+  --   ---@diagnostic disable-next-line: missing-fields
+  --   require('refactoring').setup {
+  --     vim.keymap.set('x', '<leader>re', function()
+  --       require('refactoring').refactor 'Extract Function'
+  --     end),
+  --  }
+  -- end,
 }
