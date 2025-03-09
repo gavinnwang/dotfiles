7dc37183c4348c81d4ae12cd775d8b9905e7404a
Gavin <79772347+gavinnwang@users.noreply.github.com>
2025-03-09 16:39:20 -0500

    Changes

--- nvim/.config/nvim/lua/gwang/lazy/colorscheme.lua
+++ nvim/.config/nvim/lua/gwang/lazy/colorscheme.lua
@@ -5,7 +5,7 @@ return {
     opts = {},
     init = function()
       vim.o.termguicolors = true
-      vim.cmd.colorscheme 'sonokai'
+      vim.cmd.colorscheme 'tokyonight-storm'
       vim.cmd.hi 'Comment gui=none'
     end,
   },
@@ -14,31 +14,6 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     opts = {},
   },
-  -- {
-  --   'jesseleite/nvim-noirbuddy',
-  --   dependencies = {
-  --     { 'tjdevries/colorbuddy.nvim', branch = 'dev' },
-  --   },
-  --   opts = {
-  --     transparent_background = false,
-  --     gamma = 0.85,
-  --     styles = {
-  --       comments = { italic = false },
-  --       keywords = { italic = false },
-  --       identifiers = { italic = false },
-  --       functions = { italic = false },
-  --       variables = { italic = false },
-  --     },
-  --     -- colors = {
-  --     --   primary = '#93518B',
-  --     --   secondary = '#EE579B',
-  --     -- },
-  --     colors = {
-  --       secondary = '#6EE2FF',
-  --       primary = '#267FB5',
-  --     },
-  --   },
-  -- },
   {
     'folke/tokyonight.nvim',
     event = { 'BufReadPre', 'BufNewFile' },
